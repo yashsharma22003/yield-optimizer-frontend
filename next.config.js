c2884c722c614f76f0c8e6063900a03eb092a8f9
yashsharma22003 <yashsharma22003@gmail.com>
2025-06-30 05:15:15 +0530

    final build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@coinbase/wallet-sdk'],
   webpack(config) {
-    // Fix: Treat Coinbase worker as ESM
     config.module.rules.push({
       test: /HeartbeatWorker\.js$/,
-      issuer: /@coinbase\/wallet-sdk/,
       type: 'javascript/esm',
+      resolve: {
+        fullySpecified: false,
+      },
     });
 
     return config;
